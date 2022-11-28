3c54f47fb144015a9c6b0916a279680d38ac8472
Thomas <thomas.chaudru@gmail.com>
2022-11-28 11:04:24 +0100

    All pages and craco added

---
+++ src/pages/Error.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Error = () => {
+    return (
+        <div>
+            Error works !
+        </div>
+    );
+};
+
+export default Error;
---
+++ src/pages/Housing.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Housing = () => {
+    return (
+        <div>
+            Housing works !
+        </div>
+    );
+};
+
+export default Housing;
