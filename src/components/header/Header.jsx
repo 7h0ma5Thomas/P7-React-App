0f535ed1d7b6cc471cfca2141e7ff10f898a2e76
Thomas <thomas.chaudru@gmail.com>
2022-11-30 18:06:47 +0100

    working on CSS and hidden menu for responsive

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react'
 import './header.css';
 import logo from '@/assets/HeaderLogo.svg';
 
 export default function Header() {
-    return (
+    const [setIsOpen] = useState(true)
+
+    return(
             <header>
                 <Link className='header-link-logo' to='/home'>
                     <img className='header-logo' src={logo} alt='Logo Kasa'/>
                 </Link>
-                <nav>
+                <nav className='nav-normal'>
                     <ul>
                         <li><Link className='header-link' to='/home'>Accueil</Link></li>
                         <li><Link className='header-link' to='/about'>A Propos</Link></li>
                     </ul>
-                </nav>  
+                </nav>
+                <div className='nav-hidden'>
+                    <button className='nav-button' onClick={() => setIsOpen(false)}>
+                        <nav>
+                                <ul>
+                                    <li><Link className='header-link' to='/home'>Accueil</Link></li>
+                                    <li><Link className='header-link' to='/about'>A Propos</Link></li>
+                                </ul>
+                        </nav>
+                    </button>
+                </div>    
             </header>
     );
 };
