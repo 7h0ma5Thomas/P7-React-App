9a59dd3e8c6c4dd18d2b9c7114b011ea86989bbc
Thomas <thomas.chaudru@gmail.com>
2022-11-30 11:57:48 +0100

    all elements added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import Banner from '@/components/banner/Banner';
+import Card from '@/components/card/Card';
+import * as Apartments from '@/data/Apartments.json';
 import './home.css';
+
+const allAparts = Apartments.default
 
 export default function Home() {
     return (
         <div className='body'>
             <Banner />
+            <div className='collection'>
+                {allAparts.map(({ title, id, cover}) => (
+                    <Card 
+                        title={title}
+                        id={id}
+                        cover={cover}
+                        key={title + id}
+                    />
+                ))}
+            </div>
         </div>
     );
 };
