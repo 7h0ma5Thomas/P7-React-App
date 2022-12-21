1c24f2b113f88a601b774250e2e76e8ee73b03e6
Thomas <thomas.chaudru@gmail.com>
2022-12-21 17:21:58 +0100

    working on comments

--- src/components/burgerMenu/BurgerMenu.jsx
+++ src/components/burgerMenu/BurgerMenu.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './burgerMenu.css'
 
 export default function BurgerMenu() {
-    const [isOpen, setIsOpen] = useState(false)
+  // On appelle le Hook "useState" permettant l'utilisation d'un état local à l'intérieure
+  // de la variable "isOpen" que l'on initialise sur "false" et que l'on modifiera à l'aide 
+  // de la fonction "setIsOpen" 
+  const [isOpen, setIsOpen] = useState(false)
 
+  // On utilise une condition pour déterminer l'état de notre composant
+  // Ensuite on déclare l'évenement "onClick" que l'on veut capter et on 
+  // lui passe la fonction "setIsOpen" qui nous permettra d'ouvrir ou de
+  // fermer notre élément en fonction de son état initial
   return isOpen ? (
     <div className='burgerMenu-open'>
         <button className='burgerMenu-button' onClick={() => setIsOpen(false)}>
