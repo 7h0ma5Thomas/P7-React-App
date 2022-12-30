21b40456a4e644f9a9253a361c9f6c1c6494ffbd
Thomas <thomas.chaudru@gmail.com>
2022-12-30 09:31:47 +0100

    banner's props refactored

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -4,12 +4,12 @@ import './banner.css';
 // Utilisation de props pour utiliser le composant dans plusieurs
 // pages différentes avec une image différente ou sans celle-ci, 
 // et avec un titre différent ou sans celui-ci
-export default function Banner(props) {
+export default function Banner({ pic, title }) {
   return (
     <div className='banner'>
-        <img className='banner-pic' src={props.pic} alt="Un paysage" />
-        {props.title && (
-          <h1 className='banner-title'>{props.title}</h1>
+        <img className='banner-pic' src={pic} alt="Un paysage" />
+        {title && (
+          <h1 className='banner-title'>{title}</h1>
         )}   
     </div>
   );
